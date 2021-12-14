bb7d44f4d12c498e85b77c54e419c4d257572a85
Pedro Henrique <pedrohv20fernandes@gmail.com>
2021-12-14 11:38:53 -0300

    Comentários e node vs chrome

---
+++ Estudos/Seção-2 JavaScript Basico para iniciantes/aula2 Comentários de código/comentarios.js
@@ -0,0 +1,60 @@
+// Comentarios no js são compostos por duas barras que é o comentario de linha -> // - e te o de barra e asterisco que é de blocos -> /* */
+// Comentarios são trechos de codigos, mas que são ignorados pelo motor/engine do js
+// No fim a gente usa o motor do node pra executar esse codigo, porque estamos usando a extensão code runner, ja no navegador a gente usa a engine do chrome, mas as duas são a mesma engine o motor v8
+// Comentarios são notações no codigo, uma documentação de um trecho de codigo explicando o que ele faz, etc
+console.log('olá mundo!1'); /*OUTRO COMENTARIO*/
+console.log('olá mundo!2');
+console.log('olá mundo!3');
+console.log('olá mundo!4');
+console.log('olá mundo!5');
+console.log('olá mundo!6');
+console.log('olá mundo!7');
+console.log('olá mundo!8');
+console.log('olá mundo!9');
+console.log('olá mundo!10');
+console.log('olá mundo!11');
+console.log('olá mundo!12');
+console.log('olá mundo!13');
+// OUTRO IGNORADO
+console.log('olá mundo!14');
+console.log('olá mundo!15');
+console.log('olá mundo!16');
+console.log('olá mundo!18');
+console.log('olá mundo!19');
+// console.log('olá mundo!20'); esse é ignorado
+// console.log('olá mundo!21'); esse é ignorado
+// console.log('olá mundo!22'); esse é ignorado
+
+/*  console.log('olá mundo!20'); esse é ignorado
+    console.log('olá mundo!21'); esse é ignorado
+    console.log('olá mundo!22'); esse é ignorado 
+*/ 
+console.log('Continuando');
+
+//Consegue quebrar a linha com um comentario longo: comentario de bloco
+
+/* COMENTARIO LONGO
+ COMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGO
+ COMENTARIO LONGO COMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGO
+ COMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGO
+ COMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGO
+ COMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGO
+ COMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGO
+ COMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGO
+ */
+
+//Não consegue quebrar a linha com um comentario longo: comentario de linha
+
+//COMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGO
+//  COMENTARIO LONGOCOMENTARIO LONGOCOMENTARIO LONGO
+
+// Usado  geralmente pra pibliotecas, tipo um javaDoc https://ricardospinoza.wordpress.com/2010/10/26/como-criar-javadoc/
+/**
+ * Um titulo
+ * as pessoas envolovidas: @author
+ * os parametros @param
+ * 
+ * 
+ * Texto sobre a biblioteca
+ * 
+ */
---
+++ Estudos/Seção-2 JavaScript Basico para iniciantes/aula3 Navegador vs Node (HTML + JavaScript)/js/index.js
@@ -0,0 +1,11 @@
+// Obs: a gente esta usando o code runner pra execuatar os codidos, ele usa o motor do node
+/* 
+Se executar esse comando no cmd ou aqui no vscode: 
+node "c:\Users\Pedro\Documents\GitHub\JsCursoUdemy\Estudos\Seção-2 JavaScript Basico para iniciantes\aula3 Navegador vs Node (HTML + JavaScript)\index.js"
+ele executa o codigo que esta aqui
+*/
+// console.log('Olaaaaa!!!')
+
+console.log('Script!!!');
+console.log("este trecho será exibido no console do navgeador ou se executar pelo vscode no terminal dele")
+
