678603f9c1c0f4938fa20c6bbe67318c4643d3e0
Pedro Henrique Vieira Fernandes <pedrohv20fernandes@gmail.com>
2022-04-20 15:45:11 -0300

    Aula 6 e 7

--- Estudos/Seção-2 JavaScript Basico para iniciantes/aula6 Exercícios - Const e Let/script.js
+++ Estudos/Seção-2 JavaScript Basico para iniciantes/aula6 Exercícios - Const e Let/script.js
@@ -4,19 +4,20 @@ tem ALTURA de altura e tem IMC é de IMC
 NOME nasceu em ANODENASCIMENTO
 */
 
-const NOME = 'Pedro Henrique'
-const SOBRENOME = 'Vieira Fernandes'
+const NOME = "Pedro Henrique";
+const SOBRENOME = "Vieira Fernandes";
 const ANODENASCIMENTO = 2001;
 const PESO = 84;
-const ALTURAM = 1.64
+const ALTURAM = 1.64;
 
 let imc; //peso / (altura²)
 let dataAtual;
 
-imc = PESO/(ALTURAM * ALTURAM)
-dataAtual = new Date().getFullYear()
-console.log(`${NOME + ' ' + SOBRENOME} TEM ${dataAtual - ANODENASCIMENTO} ANOS, PESA ${PESO} KG
+imc = PESO / (ALTURAM * ALTURAM);
+dataAtual = new Date().getFullYear();
+console.log(`${NOME + " " + SOBRENOME} TEM ${
+  dataAtual - ANODENASCIMENTO
+} ANOS, PESA ${PESO} KG
 TEM ${ALTURAM} DE ALTURA E TEM ${imc} É DE IMC
 ${NOME + SOBRENOME} NASCEU EM ${ANODENASCIMENTO}
-`)
-
+`);
---
+++ Estudos/Seção-2 JavaScript Basico para iniciantes/aula7 Let vs Var - Primeira diferença/script.js
@@ -0,0 +1,14 @@
+// ECMAScript -> padronização da linguagem (ES6 -> 2015) -> algum recurso que foi introduzido na linguagem e o ECMA é a documnetação/padronização
+
+nome = `Pedro` //não fazer isso -> essa variavel fica de forma global tipo a var, o ideal é criar com let ou const
+nome = `peu`
+
+var teste
+var teste
+// com let ou const eu não consigo redeclarar no mesmo trecho de codigo, mas reatribuir sim e posso iniciar vazia
+let sobrenome
+sobrenome = `Pedro`
+
+
+// Com const ja tem que iniciar com valor, não pode redeclarar, não pode reatribuir
+const CLIENTE = `Pedro`
